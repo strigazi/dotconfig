ad7bb253e5ac56541f821bc328db07cf90105a21
Spyros Trigazis <spyridon.trigazis@cern.ch>
2018-10-02 15:48:41 +0200

    Add i3 config
    Signed-off-by: Spyros Trigazis <spyridon.trigazis@cern.ch>

---
+++ i3/keyboard.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+setxkbmap -model pc105 -layout us,gr -option grp:alt_shift_toggle
---
+++ i3/laptop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pacmd set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
+
+${HOME}/.screenlayout/laptop.sh
