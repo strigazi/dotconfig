c412bff48a5d81c6a28631a2ec1d3c39c14bb49c
Spyros Trigazis <spyridon.trigazis@cern.ch>
2020-07-10 18:07:42 +0300

    Update with screenshot and zoom
    Signed-off-by: Spyros Trigazis <spyridon.trigazis@cern.ch>

--- i3/dock.sh
+++ i3/dock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-pacmd set-default-sink alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo
+#pacmd set-default-sink alsa_output.usb-Lenovo_ThinkPad_Thunderbolt_3_Dock_USB_Audio_000000000000-00.analog-stereo
 
 ${HOME}/.screenlayout/dock.sh
--- i3/laptop.sh
+++ i3/laptop.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-
-pacmd set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo
-
-${HOME}/.screenlayout/laptop.sh
+xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-1 --off --output HDMI-1 --off --output DP-1-3 --off --output DP-1-2 --off --output DP-1-1 --off
